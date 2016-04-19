0a10720e06f0820a0e3f906755d7722b0545cea7
Theodor Diaconu <diaconu.theodor@gmail.com>
2016-04-19 10:28:22 +0300

    updated role check

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'cultofcoders:quantum-framework',
-    version: '1.0.5',
+    version: '1.0.6',
     summary: 'Aggregates the quantum framework core and plugins.',
     git: 'https://github.com/cult-of-coders/quantum-framework.git',
     documentation: 'README.md'

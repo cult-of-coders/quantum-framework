0fd0e11e77643e122aeea14d64a2f7a9ffadb81d
Theodor Diaconu <diaconu.theodor@gmail.com>
2016-05-18 13:11:15 +0300

    fixed a bug with template-behavior

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'cultofcoders:quantum-framework',
-    version: '1.1.5',
+    version: '1.1.6',
     summary: 'The Quantum Framework',
     git: 'https://github.com/cult-of-coders/quantum-framework.git',
     documentation: 'README.md'
--- plugins/client/template-behavior.js
+++ plugins/client/template-behavior.js
@@ -6,20 +6,19 @@ var plugin = class extends Quantum.Model.Plugin {
     validate(config) {
         let allowedWhens = ['onCreated', 'onRendered', 'onDestroyed'];
         _.each(config, (handler, when) => {
+            check(handler, Function);
             check(when, String);
             if (!_.contains(allowedWhens, when)) {
                 throw new Meteor.Error('invalid-config', `${when} is not a valid event. Allowing only: onCreated, onRendered, onDestroyed`);
             }
-
-            check(handler, Function);
         });
     }
 
     attachBehavior(templateName, behaviorName) {
-        let behavior = this.get(behaviorName);
+        let config = this.get(behaviorName).config;
         let tpl = Template[templateName];
 
-        _.each(when, (handler, when) => {
+        _.each(config, (handler, when) => {
             tpl[when](handler);
         });
     }

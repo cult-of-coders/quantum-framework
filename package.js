62af3a10fb5f04fc1192e9a025939a7f76bad8d6
Theodor Diaconu <diaconu.theodor@gmail.com>
2016-05-25 10:52:27 +0300

    error with package publishing

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'cultofcoders:quantum-framework',
-    version: '1.1.8_3',
+    version: '1.1.8_4',
     summary: 'The Quantum Framework',
     git: 'https://github.com/cult-of-coders/quantum-framework.git',
     documentation: 'README.md'

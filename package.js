e6e98e8afc00cdf461ef421e7b048f16680e13e6
Theodor Diaconu <diaconu.theodor@gmail.com>
2016-05-31 12:56:25 +0300

    fixed bug with role tpl helper

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'cultofcoders:quantum-framework',
-    version: '1.1.8_6',
+    version: '1.1.8_7',
     summary: 'The Quantum Framework',
     git: 'https://github.com/cult-of-coders/quantum-framework.git',
     documentation: 'README.md'

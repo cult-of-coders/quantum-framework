2e74c053fc1db12570d26205b62eb6b135500140
Theodor Diaconu <diaconu.theodor@gmail.com>
2016-06-17 17:11:33 +0300

    fixed infinite recursion problem and consistencies for single static queries

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'cultofcoders:quantum-framework',
-    version: '1.1.10_1',
+    version: '1.1.10_2',
     summary: 'The Quantum Framework',
     git: 'https://github.com/cult-of-coders/quantum-framework.git',
     documentation: 'README.md'
